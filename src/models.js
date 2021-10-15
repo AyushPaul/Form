b569f97b4d2ab3bd8d8ae99d0813efff1abb01bd
Ayush Paul <ayushpaul2002@gmail.com>
2021-10-15 10:33:18 +0530

    models

---
+++ src/models.js
@@ -0,0 +1,44 @@
+const DataTypes = require('sequelize')
+const {db} = require('./connection')
+
+
+const Course = db.define('course' , {
+    id:{
+        type:DataTypes.STRING(2),
+        primaryKey:true
+    },
+    name:DataTypes.STRING(20)
+})
+
+const Teacher = db.define('teacher' , {
+    name:{
+        type:DataTypes.STRING(20),
+        allowNull:false
+    }
+})
+
+const Center = db.define('center' , {
+    id:{
+        type:DataTypes.STRING(2),
+        primaryKey:true
+    },
+    name:{
+        type:DataTypes.STRING(15),
+        allowNull:false
+    },
+    city:{
+        type:DataTypes.STRING(15),
+        allowNull:false
+    }
+})
+
+const Season = db.define('season' , {
+    id:{
+        type:DataTypes.STRING(2),
+        primaryKey:true
+    },
+    name:{
+        type:DataTypes.STRING(15),
+        allowNull:false
+    }
+})
