69b10ec3ae6be026ffc177ce84799b6931394e27
Ayush Paul <ayushpaul2002@gmail.com>
2021-10-15 11:32:19 +0530

    added center initial values

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,1 +1,23 @@
 const {db,Center,Season,Batch,Course,Teacher} = require('./models.js')
+
+const seed = async ()=>{
+    try{
+
+        db.sync({alter:true})
+
+        await Center.bulkCreate([
+            {id:'PP' , name:'PitamPura' , city:'New Delhi'},
+            {id:'DW' , name:'Dwarka' , city:'New Delhi'},
+            {id:'NO' , name:'Noida' , city:'New Delhi'},
+            {id:'DD' , name:'DehraDun' , city:'DehraDun'},
+            {id:'OL' , name:'Online' , city:'New Delhi'}
+        ],{
+            ignoreDuplicates:true
+        })
+
+    }catch(e){
+        console.error(e)
+    }
+}
+
+seed()
